210778c3067f1b0aafe85fb6184a5a4b469b341c
Rocko <codingwithrock@gmail.com>
2022-08-10 22:45:54 +0600

    two function are ready

--- assignment.js
+++ assignment.js
@@ -1,7 +1,32 @@
-echo "# basic-js-assignment" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/codingwithrock/basic-js-assignment.git
-git push -u origin main
+// * Problem ONE START
+
+// TODO Check if you need after dot values..
+function radianToDegree(radian) {
+    degree = radian * 180 / Math.PI;
+
+    return degree;
+
+}
+
+
+// console.log(radianToDegree(1));
+
+// * Problem ONE END
+
+
+
+
+// * Problem Two Start
+function isJavaScriptFile(fileName) {
+
+    if (fileName.includes('.js')) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+console.log(isJavaScriptFile("abul.js"));
+// * Problem Two End
