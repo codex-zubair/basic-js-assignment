338eb6fa0d83c86a1db8b25fb24cae4f228c7c14
Rocko <codingwithrock@gmail.com>
2022-08-10 23:43:15 +0600

    basic code done of five problems

--- assignment.js
+++ assignment.js
@@ -1,3 +1,8 @@
+// TODO add let and const 
+// TODO Remove All TODO 
+// TODO ADD COMMENTS
+
+
 // * Problem ONE START
 
 // TODO Check if you need after dot values..
@@ -35,17 +40,90 @@ function isJavaScriptFile(fileName) {
 
 
 // *Problem Three  Start
-function oilPrice(diesel, petrol, octen) {
+function oilPrice(diesel, petrol, octane) {
 
-    dieselPrice = 114 * diesel;
-    petrolPrice = 130 * petrol;
-    octenPrice = 135 * octen;
+    let dieselPrice = 114 * diesel;
+    let petrolPrice = 130 * petrol;
+    let octenPrice = 135 * octane;
 
     // Total billing
-    totalBill = dieselPrice + petrolPrice + octenPrice;
+    const totalBill = dieselPrice + petrolPrice + octenPrice;
 
     return totalBill;
 }
 
 // console.log(oilPrice(2, 1, 2));
 // *Problem Three  Start
+
+
+
+// TODO Check the Video Of this Section and Full fill the requirement
+// *Problem Four Start
+function publicBusFare(passenger) {
+
+
+    // * Sending passenger to bus
+    if (passenger % 50 != 0) {
+
+        let extraPassengerOfBus = passenger % 50;
+        console.log(extraPassengerOfBus);
+
+        // * Sending passenger to microbus
+        if (extraPassengerOfBus > 11) {
+
+            let extraPassengerOfMicro = extraPassengerOfBus % 11;
+
+            console.log(extraPassengerOfMicro);
+
+
+
+            // * Checking Extra passenger are exist from Micro bus or not
+            if (extraPassengerOfMicro != 0) {
+
+                // * Calculating The Bus Fair For Extra People...
+                const busFair = extraPassengerOfMicro * 250;
+
+                return busFair;
+            }
+
+
+        }
+
+    }
+
+
+}
+
+// console.log(publicBusFare(58))
+
+// *Problem Four End
+
+
+
+
+//*problem five Start
+function isBestFriend(obj1, obj2) {
+    if (obj1.foo == obj2.bar) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+
+let someObject1 = {
+    foo: "Tom",
+    bar: "Rock"
+};
+
+let someObject2 = {
+    foo: "Rock",
+    bar: "Tm"
+};
+
+
+console.log(isBestFriend(someObject1, someObject2));
+
+//*problem five End
